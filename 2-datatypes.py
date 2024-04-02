f8c006402673239a5db131629fe37b65b2eb495d
Balaji B <ji.balajib@gmail.com>
2024-04-02 22:33:58 +0530

    datatypes added

---
+++ 2-datatypes.py
@@ -0,0 +1,48 @@
+#case sensitive
+amrita = 1
+Amrita  = 1
+
+# variable can start with _ but not a number
+
+price = 20.50
+unit = 10
+
+total = price*unit
+
+#order of typecasting  - int < float
+# Boolean
+amrita = True
+Amrita = False
+
+number = 1
+print(number)
+print(Amrita)
+
+
+# operation on string
+sample = 'good day'
+print(sample.upper())
+sample = 'GOOD DAY'
+print(sample.lower())
+
+
+# data validation
+name = 'aifoss'
+number = 123456
+symbol = '$'
+
+print(name.isalpha()) # check for alphabets
+print(name.isnumeric()) # check for numbers
+print(name.isalnum()) # check for alphabets and numbers
+
+
+print('check symbol')
+print(symbol.isalnum())
+
+sample_string = 'a quick brown fox'
+# try to findout what the following syntax does
+# startswith
+# endswith
+# find
+# index
+
