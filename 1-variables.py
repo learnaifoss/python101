8c7d59b7ded3ba63c72441bf8708d72392f6da6f
Balaji B <ji.balajib@gmail.com>
2024-04-02 21:41:05 +0530

    variables complete

--- 1-variables.py
+++ 1-variables.py
@@ -1,1 +1,30 @@
 # this file explains variables in python
+
+print ('My Blog is AIFOSS')
+
+print ('2+3')
+
+# + addition
+# - Subtraction
+# /(float division) 
+# //(integer division)
+# * multiply
+# % modulo - remainder
+
+print(4//2)
+
+# type is a function to return the type of variable
+number = 1 #int
+print(type(number))
+
+number = float(number) #float
+print(type(number))
+
+number = int(number) #int
+print(type(number))
+#print(type(1.1))
+
+
+# find the syntax for floor and ceil equivalent functions in c
+
+
